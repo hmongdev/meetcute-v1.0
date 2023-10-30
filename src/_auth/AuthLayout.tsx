6dc04e2a629a9e1b4f4cfe8d4a49ca8f49c3cf37
steve <steveyangwork@gmail.com>
2023-10-30 15:39:35 -0500

    add image to layout

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -10,9 +10,11 @@ const AuthLayout = () => {
         <Navigate to="/"/>
       ) : (
           <>
-            <section>
+            <section className="flex flex-1 justify-center items-center flex-col py-10">
               <Outlet />
             </section>
+            
+            <img src="/assets/images/side-img.svg" alt="logo" className="hidden md:block h-screen w-1/2 object-cover bg-no-repeat"/>
             </>
     )}  
     </>
